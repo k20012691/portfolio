33eb9f4484e86b8da895f522ad58f7107341c951
Vedeesh Bali <vedeeshb18@gmail.com>
2024-05-26 10:19:54 +0100

    configure hashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
 import About from './pages/About';
@@ -9,14 +9,14 @@ import Photography from './pages/Photography';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/about/' element={<About />} />
         <Route path='/about/:photography/' element={<Photography />} />
         <Route path='/projects/' element={<Portfolio />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
